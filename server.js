dd648240bab4974ca82625f26e5c1d4f339c9935
gkf <gkf2103@gmail.com>
2021-01-26 20:24:50 -0500

    basic server

---
+++ server.js
@@ -0,0 +1,24 @@
+// Require/import the HTTP module
+var http = require("http");
+var express = require ("express")
+var path = require ("path")
+
+// sets up the Express APP
+var app = express();
+var PORT = 3000;
+
+// Require/import the HTTP module
+var http = require("http");
+
+
+
+// Create a generic function to handle requests and responses
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
