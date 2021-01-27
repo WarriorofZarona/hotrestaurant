024bc35b7a06a905dce4ab0cdf9b8ea1180f2eec
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-26 21:48:43 -0500

    completed server.js

--- server.js
+++ server.js
@@ -6,7 +6,26 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set("view", path.join(__dirname, "views"));
+
+app.post("/form", (req, res) => {
+  const { name, phone, email, uniqueID } = req.body;
+
+  const newReservation = {
+    customerName: name,
+    phoneNumber: phone,
+    customerEmail: email,
+    customerID: uniqueID,
+  };
+
+  for (var i = 0; i >= apiTables.length; i++) {
+    if (apiTables.length > 5) {
+      waitList.push(newReservation);
+    } else {
+      apiTables.push(newReservation);
+    }
+  }
+  res.json(newReservation);
+});
 
 let apiTables = [
   {
@@ -26,23 +45,13 @@ let waitList = [
   },
 ];
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-app.get("/reserve", function (req, res) {
-  res.sendFile(path.join(__dirname, "form.html"));
-});
-app.get("/tables", function (req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
+app.get("/view", (req, res) => res.sendFile(path.join(__dirname, "view.html")));
+app.get("/form", (req, res) => res.sendFile(path.join(__dirname, "form.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get("/api/tables", function (req, res) {
-  return res.json(apiTables);
-});
+app.get("/api/tables", (req, res) => res.json(apiTables));
 
-app.get("/api/waitlist", function (req, res) {
-  return res.json(waitList);
-});
+app.get("/api/waitlist", (req, res) => res.json(waitList));
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
